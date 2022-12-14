299c4ad25a478c981b1e506766c7e0f5db88d672
Eugene848 <m204isachenko@gmail.com>
2022-12-14 19:17:43 +0300

    Weather informer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="informer-container">
+    <TemperatureComponent v-bind:real="real" v-bind:feel="feel"></TemperatureComponent>
+    <div class="flex-break"></div>
+    <HumidityComponent v-bind:humidity="humidity"></HumidityComponent>
+    <div class="vertical-divider"></div>
+    <WindComponent v-bind:wind="wind"></WindComponent>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TemperatureComponent from './components/TemperatureComponent.vue'
+import HumidityComponent from './components/HumidityComponent.vue'
+import WindComponent from './components/WindComponent.vue'
 
-export default {
-  name: 'App',
+export default{
+  name: 'app',
   components: {
-    HelloWorld
+    TemperatureComponent, 
+    HumidityComponent,
+    WindComponent
+  },
+  data(){
+  return {
+    real: 1,
+    feel: '-3.2',
+    humidity: 6,
+    wind: 20
   }
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
 }
+#app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: rgb(244, 123, 186);
+  background: linear-gradient(-25deg, rgba(244, 123, 186, 0.5) 20%, rgba(90, 95, 245, 0.5) 80%);
+}
+
+.informer-container {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 400px;
+  padding: 30px;
+  background-image: url('img/forest.png');
+  background-size: cover;
+  background-position: center center;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.75);
+}
+
+.flex-break{
+  flex-basis: 100%;
+  height: 0;
+}
+
+.vertical-divider{
+  height: 70px;
+  width: 1px;
+  background-color: #fff;
+  vertical-align: middle;
+  display: table-cell;
+}
+
 </style>
---
+++ src/components/WindComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="WindComponent" class="wind">
+        <img src="../img/wind.png"> {{wind}} <small>м/с</small>
+      </div>
+</template>
+
+<script>
+export default{
+  name: 'HumidityComponent',
+  props: ['wind'],
+  data(){}
+}
+</script>
+
+<style>
+
+.wind {
+  font-family: 'Arial', sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  color: #fff;
+  position: relative;
+  display: inline-block;
+  line-height: 0.9;
+  text-shadow: 2px 2px 2px #444;
+  margin: 15px;
+}
+
+.wind img {
+  width: 30px;
+  margin-right: -5px;
+  vertical-align: middle;
+}
+
+</style>
